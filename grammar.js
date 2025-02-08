bd0d545073017e33f326498e35cdae836647cf74
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2025-02-08 03:09:03 +0200

    Update grammar.js

--- grammar.js
+++ grammar.js
@@ -98,7 +98,6 @@ module.exports = grammar({
         ']'
       ),
 
-    // https://help.adobe.com/fr_FR/as3/learn/WS5b3ccc516d4fbf351e63e3d118a9b90204-7f36.html
     class_attribut: ($) => choice('dynamic', 'final', 'internal', 'public'),
 
     interface_declaration: ($) =>
@@ -113,7 +112,6 @@ module.exports = grammar({
         '}'
       ),
 
-    //https://help.adobe.com/en_US/as3/learn/WS5b3ccc516d4fbf351e63e3d118a9b90204-7f41.html
     interface_attribut: ($) => choice('internal', 'public'),
 
     method_declaration: ($) =>
@@ -184,7 +182,6 @@ module.exports = grammar({
         ';'
       ),
 
-    // https://help.adobe.com/fr_FR/as3/learn/WS5b3ccc516d4fbf351e63e3d118a9b90204-7f36.html
     property_attribut: ($) =>
       choice(
         'internal',
